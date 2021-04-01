2b9439a00c83435008c15f740f0259d8906d12ba
Mariya Ryabova <mariryabova2001@gmail.com>
2021-04-01 15:16:29 +0500

    Результат

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,9 +6,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-           int a=Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
+          
             var calculator = new CalculatorLib.Calculator(10);
+            Console.WriteLine("Введите число а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сумма равна ");
             Console.WriteLine(calculator.Add(a,b));
         }
     }
