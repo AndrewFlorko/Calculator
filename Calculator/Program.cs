7c4c846c01c1ffe2a6e02ac38c6cd21735caf038
Mariya Ryabova <mariryabova2001@gmail.com>
2021-04-01 15:08:55 +0500

    Сложение

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,10 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            
+           int a=Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            var calculator = new CalculatorLib.Calculator(10);
+            Console.WriteLine(calculator.Add(a,b));
         }
     }
 }
